43b7ed87175d68891b739e83843bb507e41c3f17
maxiim1098 <156887699+maxiim1098@users.noreply.github.com>
2024-01-17 18:13:41 +1000

    Add files via upload

---
+++ script2.ts
@@ -0,0 +1,5 @@
+const a:number = 1
+const b:string = "1"
+const c:string[] = ["a", "b", "c"]
+const c1:any[] = ["a", "b", "c"] //типизация массива при неизвестных данных
+const arrow:() =>void = ()=> {} //типизация стрелочной функции
